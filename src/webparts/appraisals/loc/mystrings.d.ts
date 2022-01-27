4bd9a903379145aef0c31dbc70f4ca4ffb751d18
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2022-01-27 14:34:49 +0200

    first

---
+++ src/webparts/appraisals/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAppraisalsWebPartStrings {
+    PropertyPaneDescription: string;
+    BasicGroupName: string;
+    DescriptionFieldLabel: string;
+}
+
+declare module 'AppraisalsWebPartStrings' {
+    const strings: IAppraisalsWebPartStrings;
+    export = strings;
+}
